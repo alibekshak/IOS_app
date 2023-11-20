119ee1d484fd522a21a5220bf1a5999b1e84efa2
alibekshak <alibekshakiro1@gmail.com>
2023-11-20 16:16:37 +0600

    Creating app for IOS

---
+++ ProjectP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectP
+//
+//  Created by Apple on 20.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextView!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
