be98bb3e1d464d1d56878bae4651c690a59677e7
alibekshak <alibekshakiro1@gmail.com>
2023-11-20 16:23:29 +0600

    Creating view and adding to view scoreLabel

--- ProjectP/ViewController.swift
+++ ProjectP/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController {
     var scoreLabel: UILabel!
     var letterButtons = [UIButton]()
     
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
